317d41e73478119b771a1791f3c1031975e2cb60
eliot <eliot@DESKTOP-JK4U576>
2017-05-11 23:25:41 +0100

    initial camera movement

--- Blocky/Entities/Camera.cs
+++ Blocky/Entities/Camera.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input.Touch;
 
 namespace Blocky.Entities
 {
+    using System;
     using Microsoft.Xna.Framework.Input;
 
     public class Camera
@@ -13,8 +14,11 @@ namespace Blocky.Entities
         // Let's start at X = 0 so we're looking at things head-on
         private Vector3 position = new Vector3(0, -20, 10);
 
-        private float angle;
+        private float angleZ;
+        private float anglevertical;
         private MouseState mouseState;
+        private Vector3 upVector;
+        private Vector3 direction;
 
         public RotationStates RotationStates { get; } = new RotationStates();
 
@@ -22,17 +26,15 @@ namespace Blocky.Entities
         {
             get
             {
-                var lookAtVector = new Vector3(0, 1, -0.3f);
-                // We'll create a rotation matrix using our angle
-                var rotationMatrix = Matrix.CreateRotationZ(angle);
+                var lookAtVector = direction;
+                // We'll create a rotation matrix using our angleZ
+                var rotationMatrix = Matrix.CreateRotationZ(angleZ);
+                
                 // Then we'll modify the vector using this matrix:
                 lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
                 lookAtVector += position;
 
-                var upVector = Vector3.UnitZ;
-
-                return Matrix.CreateLookAt(
-                    position, lookAtVector, upVector);
+                return Matrix.CreateLookAt(position, lookAtVector, upVector);
             }
         }
 
@@ -42,7 +44,7 @@ namespace Blocky.Entities
             {
                 float fieldOfView = MathHelper.PiOver4;
                 float nearClipPlane = 1;
-                float farClipPlane = 200;
+                float farClipPlane = 500;
                 float aspectRatio = graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
 
                 return Matrix.CreatePerspectiveFieldOfView(
@@ -55,6 +57,8 @@ namespace Blocky.Entities
             this.graphicsDevice = graphicsDevice;
             Mouse.SetPosition(graphicsDevice.Viewport.Width/2, graphicsDevice.Viewport.Height/2);
             mouseState = Mouse.GetState();
+            upVector = Vector3.UnitZ;
+            direction = new Vector3(0, 1, -0.3f);
         }
 
         public void Update(GameTime gameTime)
@@ -63,56 +67,52 @@ namespace Blocky.Entities
 
             var state = Keyboard.GetState();
 
+            var changeVector = new Vector3();
+
             if (state.IsKeyDown(Keys.W))
-                position.Y += speed;
+                changeVector.Y = 1;
             if (state.IsKeyDown(Keys.S))
-                position.Y -= speed;
+                changeVector.Y = -1;
             if (state.IsKeyDown(Keys.A))
             {
-                position.X -= speed;
+                changeVector.X = -1;
             }
             if (state.IsKeyDown(Keys.D))
             {
-                position.X += speed;
+                changeVector.X = 1;
             }
 
             var currentMouseState = Mouse.GetState();
 
             RotationStates.X = currentMouseState.X - mouseState.X;
+            RotationStates.Y = currentMouseState.Y - mouseState.Y;
 
-            TouchCollection touchCollection = TouchPanel.GetState();
-
-            bool isTouchingScreen = touchCollection.Count > 0;
-            if (isTouchingScreen)
+            if (RotationStates.Y < -0.5)
             {
-                var xPosition = touchCollection[0].Position.X;
-
-                float xRatio = xPosition / graphicsDevice.Viewport.Width;
-
-                if (xRatio < 1 / 3.0f)
-                {
-                    angle += (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-                else if (xRatio < 2 / 3.0f)
-                {
-                    var forwardVector = new Vector3(0, -1, 0);
-
-                    var rotationMatrix = Matrix.CreateRotationZ(angle);
-                    forwardVector = Vector3.Transform(forwardVector, rotationMatrix);
-
-                    const float unitsPerSecond = 3;
-
-                    position += forwardVector * unitsPerSecond *
-                        (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-                else
-                {
-                    angle -= (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
+                anglevertical += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else if(RotationStates.Y > 0.5f)
+            {
+                anglevertical -= (float) gameTime.ElapsedGameTime.TotalSeconds;
             }
 
+            if (RotationStates.X < -0.5f)
+            {
+                angleZ += (float)gameTime.ElapsedGameTime.TotalSeconds * Math.Abs(RotationStates.X)/25;
+            }
+            else if (RotationStates.X > 0.5f)
+            {
+                angleZ -= (float) gameTime.ElapsedGameTime.TotalSeconds*Math.Abs(RotationStates.X)/25;
+            }
+
+            var rotationMatrix = Matrix.CreateRotationZ(angleZ);
+            var forwardVector = Vector3.Transform(changeVector, rotationMatrix * 2);
+            
+            position += forwardVector;
+            
             Mouse.SetPosition(graphicsDevice.Viewport.Width/2, graphicsDevice.Viewport.Height/2);
             mouseState = Mouse.GetState();
         }
+        
     }
 }
