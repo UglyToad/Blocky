5acb3597cae1ae0462bbff933b6fb85f0c8e771e
eliot <eliot@DESKTOP-JK4U576>
2017-05-11 21:58:34 +0100

    initial tutorial code

---
+++ Blocky/Abstractions/IContentManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Blocky.Abstractions
+{
+    public interface IContentManager
+    {
+        T Load<T>(string name);
+    }
+
+    public class ContentManager : IContentManager
+    {
+        private readonly Microsoft.Xna.Framework.Content.ContentManager contentManager;
+
+        public ContentManager(Microsoft.Xna.Framework.Content.ContentManager contentManager)
+        {
+            this.contentManager = contentManager;
+        }
+
+        public T Load<T>(string name)
+        {
+            return contentManager.Load<T>(name);
+        }
+    }
+}
